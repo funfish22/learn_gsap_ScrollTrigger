da8ef356801e9c0627ce5b48b6cdbd608bccbafd
t_aga <aga.lhj@link8tech.tw>
2022-12-13 10:58:38 +0800

    調整代碼

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,65 +10,121 @@ var headphoneCanvas = document.querySelector(".headphone");
 const context = headphoneCanvas.getContext("2d");
 let headphoneProportion = 800 / 536;
 
-gsap.to(".box--left", {
-	opacity: 1,
-	x: "0",
-	scrollTrigger: {
-		trigger: ".section1", // 觸發選擇器
-		start: "top 70%", // 當觸發選擇器的頂端 (左邊的 top) 碰到檢視區頂端 (右邊的 top) 時啟用
-		end: "top 40%", // 當觸發選擇器的頂端 (左邊的 top) 碰到檢視區頂端 (右邊的 top) 時啟用, 也可以使用移動幾px end: "+=100",
-		scrub: 1, // 是否根據捲動程度決定動畫完成程度，如果填寫數字，則在會延遲 n 秒後才到定位
-	},
-});
-gsap.to(".box--center", {
-	opacity: 1,
-	scrollTrigger: {
-		trigger: ".section1",
-		start: "top 60%",
-		end: "+=100",
-		scrub: !0,
-		// pin: true,  // 當動畫執行中的時候，讓觸發選擇器位置固定住
-	},
-});
-gsap.to(".box--right", {
-	opacity: 1,
-	x: "0",
-	scrollTrigger: {
-		trigger: ".section1",
-		start: "top 50%",
-		end: "top 40%",
-		scrub: 1, // 是否根據捲動程度決定動畫完成程度，如果填寫數字，則在會延遲 n 秒後才到定位
-	},
-});
+window.onload = function () {
+	gsap.to(".box--left", {
+		opacity: 1,
+		x: "0",
+		scrollTrigger: {
+			trigger: ".section1", // 觸發選擇器
+			start: "top 70%", // 當觸發選擇器的頂端 (左邊的 top) 碰到檢視區頂端 (右邊的 top) 時啟用
+			end: "top 40%", // 當觸發選擇器的頂端 (左邊的 top) 碰到檢視區頂端 (右邊的 top) 時啟用, 也可以使用移動幾px end: "+=100",
+			scrub: 1, // 是否根據捲動程度決定動畫完成程度，如果填寫數字，則在會延遲 n 秒後才到定位
+		},
+	});
+	gsap.to(".box--center", {
+		opacity: 1,
+		scrollTrigger: {
+			trigger: ".section1",
+			start: "top 60%",
+			end: "+=100",
+			scrub: !0,
+			// pin: true,  // 當動畫執行中的時候，讓觸發選擇器位置固定住
+		},
+	});
+	gsap.to(".box--right", {
+		opacity: 1,
+		x: "0",
+		scrollTrigger: {
+			trigger: ".section1",
+			start: "top 50%",
+			end: "top 40%",
+			scrub: 1, // 是否根據捲動程度決定動畫完成程度，如果填寫數字，則在會延遲 n 秒後才到定位
+		},
+	});
 
-gsap.timeline({
-	scrollTrigger: {
-		trigger: ".circleRoot",
-		start: "top 80%",
-		end: "top 20%",
-		scrub: !0,
-	},
-	ease: "strong.inOut",
-})
-	.fromTo(".circle--left .circleInner", { x: 100 }, { x: 0, opacity: 1 })
-	.fromTo(".circle--center .circleInner", { x: 200 }, { x: 0, opacity: 1 })
-	.fromTo(".circle--right .circleInner", { x: 300 }, { x: 0, opacity: 1 });
+	gsap.timeline({
+		scrollTrigger: {
+			trigger: ".circleRoot",
+			start: "top 70%",
+			end: "bottom 100%",
+			scrub: !0,
+		},
+		ease: "strong.inOut",
+	})
+		.fromTo(".circle--left .circleInner", { x: 100 }, { x: 0, opacity: 1 })
+		.fromTo(
+			".circle--center .circleInner",
+			{ x: 200 },
+			{ x: 0, opacity: 1 }
+		)
+		.fromTo(
+			".circle--right .circleInner",
+			{ x: 300 },
+			{ x: 0, opacity: 1 }
+		);
 
-gsap.timeline({
-	scrollTrigger: {
-		trigger: ".section3",
-		start: "top 0%",
-		end: "+=3000",
-		scrub: !0,
-		pin: true, // 當動畫執行中的時候，讓觸發選擇器位置固定住
-	},
-	ease: "strong.inOut",
-}).fromTo(".x-axis", { x: 0 }, { x: "-75%" });
+	gsap.timeline({
+		scrollTrigger: {
+			trigger: ".section3",
+			start: "top 0%",
+			end: "+=3000",
+			scrub: !0,
+			pin: true, // 當動畫執行中的時候，讓觸發選擇器位置固定住
+		},
+		ease: "strong.inOut",
+	}).fromTo(".x-axis", { x: 0 }, { x: "-75%" });
 
-vid.addEventListener("loadedmetadata", () => {
+	// vid.addEventListener("loadedmetadata", () => {
+	// 	scrollHeight = Math.floor(vid.duration) * playbackConst;
+	// 	// section4.style.height = Math.floor(vid.duration) * playbackConst + "px";
+	// 	videoTop = section4.offsetTop;
+	// 	gsap.timeline({
+	// 		scrollTrigger: {
+	// 			trigger: ".section4",
+	// 			start: "top 0%",
+	// 			end: `+=${scrollHeight}`,
+	// 			scrub: !0,
+	// 			pin: true, // 當動畫執行中的時候，讓觸發選擇器位置固定住
+	// 			onEnter: () => {
+	// 				start();
+	// 			},
+	// 			onEnterBack: () => {
+	// 				start();
+	// 			},
+	// 			onLeaveBack: () => {
+	// 				stop();
+	// 			},
+	// 			onLeave: () => {
+	// 				stop();
+	// 			},
+	// 		},
+	// 	});
+
+	// 	function scrollPlay() {
+	// 		requestId = undefined;
+	// 		var frameNumber = (window.pageYOffset - videoTop) / playbackConst;
+	// 		vid.currentTime = frameNumber;
+	// 		start();
+	// 	}
+
+	// 	function start() {
+	// 		var frameNumber = (window.pageYOffset - videoTop) / playbackConst;
+	// 		vid.currentTime = frameNumber;
+	// 		if (!requestId) {
+	// 			requestId = window.requestAnimationFrame(scrollPlay);
+	// 		}
+	// 	}
+
+	// 	function stop() {
+	// 		if (requestId) {
+	// 			window.cancelAnimationFrame(requestId);
+	// 			requestId = undefined;
+	// 		}
+	// 	}
+	// });
+
 	scrollHeight = Math.floor(vid.duration) * playbackConst;
 	// section4.style.height = Math.floor(vid.duration) * playbackConst + "px";
-	videoTop = section4.offsetTop;
 	gsap.timeline({
 		scrollTrigger: {
 			trigger: ".section4",
@@ -90,6 +146,29 @@ vid.addEventListener("loadedmetadata", () => {
 			},
 		},
 	});
+
+	function scrollPlay() {
+		videoTop = section4.parentElement.offsetTop;
+		requestId = undefined;
+		var frameNumber = (window.pageYOffset - videoTop) / playbackConst;
+		vid.currentTime = frameNumber;
+		start();
+	}
+
+	function start() {
+		var frameNumber = (window.pageYOffset - videoTop) / playbackConst;
+		vid.currentTime = frameNumber;
+		if (!requestId) {
+			requestId = window.requestAnimationFrame(scrollPlay);
+		}
+	}
+
+	function stop() {
+		if (requestId) {
+			window.cancelAnimationFrame(requestId);
+			requestId = undefined;
+		}
+	}
 
 	const frameCount = 56;
 	const currentFrame = (index) =>
@@ -145,7 +224,7 @@ vid.addEventListener("loadedmetadata", () => {
 		{
 			display: "block",
 			scale: 5,
-			duration: 1
+			duration: 1,
 		},
 		"section5Phone"
 	);
@@ -155,10 +234,10 @@ vid.addEventListener("loadedmetadata", () => {
 		{
 			keyframes: {
 				"3%": { opacity: 1 },
-				"50%": {scale: 120}
+				"50%": { scale: 120 },
 			},
 			display: "block",
-			duration: 1
+			duration: 1,
 		},
 		"section5Phone"
 	);
@@ -172,7 +251,7 @@ vid.addEventListener("loadedmetadata", () => {
 			},
 			display: "block",
 			scale: 10,
-			duration: 1
+			duration: 1,
 		},
 		"section5Phone"
 	);
@@ -214,29 +293,7 @@ vid.addEventListener("loadedmetadata", () => {
 			1400 / headphoneProportion
 		);
 	}
-});
-
-function scrollPlay() {
-	requestId = undefined;
-	var frameNumber = (window.pageYOffset - videoTop) / playbackConst;
-	vid.currentTime = frameNumber;
-	start();
-}
-
-function start() {
-	var frameNumber = (window.pageYOffset - videoTop) / playbackConst;
-	vid.currentTime = frameNumber;
-	if (!requestId) {
-		requestId = window.requestAnimationFrame(scrollPlay);
-	}
-}
-
-function stop() {
-	if (requestId) {
-		window.cancelAnimationFrame(requestId);
-		requestId = undefined;
-	}
-}
+};
 
 // headphoneCanvas.width = 800;
 // headphoneCanvas.height = 536;
